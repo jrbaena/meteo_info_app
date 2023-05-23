6598fb4d9514f44802f9384d5d41e8bc3e4e9a4e
Jose Rodriguez <j.rodriguez@tvup.media>
2023-05-23 18:21:12 +0200

    feat(support): add string extension and create method for replace special characters

---
+++ lib/extensions/string_extension.dart
@@ -0,0 +1,14 @@
+extension StringExtension on String {
+  String replaceSpecialCharacters() {
+    return replaceAll("á", "a")
+        .replaceAll("é", "e")
+        .replaceAll("í", "i")
+        .replaceAll("ó", "o")
+        .replaceAll("ú", "u")
+        .replaceAll("Á", "a")
+        .replaceAll("É", "e")
+        .replaceAll("Í", "i")
+        .replaceAll("Ó", "o")
+        .replaceAll("Ú", "u");
+  }
+}
